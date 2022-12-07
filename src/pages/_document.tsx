9ea5930bdd63b5fcae2a1b81e85f5cc4ba8397f7
yasincandev <canyasin1996@gmail.com>
2022-12-07 13:38:33 +0300

    meta tag improvments

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,11 @@
-import Document, { DocumentContext } from "next/document";
+import Document, {
+  DocumentContext,
+  Html,
+  Main,
+  NextScript,
+  Head,
+} from "next/document";
+
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
@@ -21,5 +28,55 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta charSet='utf-8' />
+          <meta name='viewport' content='width=device-width, initial-scale=1' />
+          <meta name='theme-color' content='#000000' />
+          <link rel='apple-touch-icon' href='/assets/apple-touch-icon.png' />
+          <meta
+            name='description'
+            content="Hi. I'm Yasin. I'm a front end developer.I build things for the web."
+          />
+          <meta property='og:url' content='https://yasincan.me' />
+          <meta property='og:title' content='Yasin Can | Front End Developer' />
+          <meta
+            property='og:description'
+            content="Hi. I'm Yasin. I'm a front end developer.I build things for the web."
+          />
+          <meta property='og:image' content='/assets/meta-image.png' />
+
+          <meta name='twitter:card' content='summary_large_image' />
+          <meta name='twitter:url' content='https://yasincan.me' />
+          <meta
+            name='twitter:title'
+            content='Yasin Can | Front End Developer'
+          />
+          <meta
+            name='twitter:description'
+            content="Hi. I'm Yasin. I'm a front end developer.I build things for the web."
+          />
+          <meta name='twitter:image' content='/images/meta-image.png' />
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link
+            rel='preconnect'
+            href='https://fonts.gstatic.com'
+            crossOrigin='true'
+          />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700&family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,600;1,700;1,900&display=swap'
+            rel='stylesheet'
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
