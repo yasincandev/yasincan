f5527a111b5761c03dac5cdf30a3b1ab42767bc7
yasincandev <canyasin1996@gmail.com>
2022-12-07 12:32:18 +0300

    about text changes

--- src/components/About/AboutPage.tsx
+++ src/components/About/AboutPage.tsx
@@ -15,10 +15,12 @@ const AboutPage: FC = () => {
           stay up to date. I&#39;m a social person and a team player. I like to
           take initiative and responsibility to inspire others. I like to
           communicate and I don&#39;t hesitate to acknowledge my mistakes. I
-          listen carefully and I love to explain things to people. I have always
-          been interested in computers since my childhood. Doing something
-          related to computers has always been in me and in 2022 I made a
-          decision and stepped on this path.So far, I can say that I have
+          listen carefully and I love to explain things to people.
+        </AboutMeText>
+        <AboutMeText>
+          I have always been interested in computers since my childhood. Doing
+          something related to computers has always been in me and in 2022 I
+          made a decision and stepped on this path.So far, I can say that I have
           learned everything by researching on my own. I am always improving
           myself and I am open to innovations.
         </AboutMeText>
