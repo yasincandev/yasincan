f1d428281a614230b222b2c8a8e4ef88162dad07
yasincandev <canyasin1996@gmail.com>
2022-12-07 13:59:05 +0300

    document and meta changes

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -12,6 +12,7 @@ import {
   MainContainer,
 } from "./styles";
 import * as mainImage from "../../public/assets/main.png";
+import Head from "next/head";
 
 type LayoutProps = {
   children: ReactNode;
@@ -23,6 +24,18 @@ const Layout: React.FunctionComponent<LayoutProps> = ({ children }) => {
   return (
     <ThemeProvider theme={currentTheme}>
       <GlobalStyles />
+      <Head>
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta name='theme-color' content='#000000' />
+        <title>Yasin Can | Front End Developer</title>
+        <meta
+          name='description'
+          content="Hi. I'm Yasin. I'm a front end developer.I build things for the web."
+        />
+        <meta property='og:url' content='https://yasincan.me' />
+        <meta property='og:title' content='Yasin Can | Front End Developer' />
+      </Head>
       <Container>
         <Navbar />
         <ContentContainer>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -34,9 +34,6 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta charSet='utf-8' />
-          <meta name='viewport' content='width=device-width, initial-scale=1' />
-          <meta name='theme-color' content='#000000' />
           <link rel='apple-touch-icon' href='/assets/apple-touch-icon.png' />
           <meta
             name='description'
