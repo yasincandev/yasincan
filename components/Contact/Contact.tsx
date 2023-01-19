ce1e7be7075eb964f58141d20c791bcd353ee74e
Yasin Can <canyasin1996@gmail.com>
2023-01-19 21:00:57 +0300

    cv file changed

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -1,19 +1,19 @@
-import Link from 'next/link'
-import { FC } from 'react'
-import { GithubIcon } from '../Icons/Github'
-import { LinkedinIcon } from '../Icons/Linkedin'
-import { ResumeIcon } from '../Icons/Resume'
-import { Container, LinkContainer, LinkText } from './styles'
-import { MediumIcon } from '../Icons/Medium'
+import Link from "next/link";
+import { FC } from "react";
+import { GithubIcon } from "../Icons/Github";
+import { LinkedinIcon } from "../Icons/Linkedin";
+import { ResumeIcon } from "../Icons/Resume";
+import { Container, LinkContainer, LinkText } from "./styles";
+import { MediumIcon } from "../Icons/Medium";
 
 const Contact: FC = () => {
   return (
     <Container>
       <LinkContainer>
         <Link
-          href="https://linkedin.com/in/yasincandev"
-          target="_blank"
-          style={{ textDecoration: 'none' }}
+          href='https://linkedin.com/in/yasincandev'
+          target='_blank'
+          style={{ textDecoration: "none" }}
         >
           <LinkedinIcon />
           <LinkText>Let&#39;s Connect! </LinkText>
@@ -22,9 +22,9 @@ const Contact: FC = () => {
 
       <LinkContainer>
         <Link
-          href="https://github.com/yasincandev"
-          target="_blank"
-          style={{ textDecoration: 'none' }}
+          href='https://github.com/yasincandev'
+          target='_blank'
+          style={{ textDecoration: "none" }}
         >
           <GithubIcon />
           <LinkText>Review My Code</LinkText>
@@ -32,9 +32,9 @@ const Contact: FC = () => {
       </LinkContainer>
       <LinkContainer>
         <Link
-          href="https://drive.google.com/file/d/1OxmrpVn40hrvqBhuluor-wMoVan1xEuj/view?usp=sharing"
-          target="_blank"
-          style={{ textDecoration: 'none' }}
+          href='https://drive.google.com/file/d/18-ciaK69f05UlEUF7crWjYCfLvi3L7_N/view?usp=sharing'
+          target='_blank'
+          style={{ textDecoration: "none" }}
         >
           <ResumeIcon />
           <LinkText>Check Out My Resume</LinkText>
@@ -42,16 +42,16 @@ const Contact: FC = () => {
       </LinkContainer>
       <LinkContainer>
         <Link
-          href="https://medium.com/@yasincandev"
-          target="_blank"
-          style={{ textDecoration: 'none' }}
+          href='https://medium.com/@yasincandev'
+          target='_blank'
+          style={{ textDecoration: "none" }}
         >
           <MediumIcon />
           <LinkText>My Blog</LinkText>
         </Link>
       </LinkContainer>
     </Container>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
