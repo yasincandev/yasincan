36e823c276564c9d605c21903ed4e11ed67e0a23
Yasin Can <canyasin1996@gmail.com>
2023-02-04 01:17:31 +0300

    changes

--- components/Contact/__test__/Contact.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render } from '@testing-library/react'
-import Contact from '../Contact'
-
-it('should render Contact component', () => {
-  const { container } = render(<Contact />)
-  expect(container).toMatchSnapshot()
-})
--- components/Main/__test__/Main.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react'
-import Main from '../Main'
-
-jest.mock('next/image', () => () => null)
-
-it('should render Main component', () => {
-  const { container } = render(<Main />)
-  expect(container).toMatchSnapshot()
-})
